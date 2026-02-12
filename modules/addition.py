6c6fd6b93547aee48d10e895b1a40df2e4d16831
Lydia Khasoa <khasoalydia@gmail.com>
2026-02-12 20:39:46 +0300

    Added modules folder

---
+++ modules/addition.py
@@ -0,0 +1,10 @@
+#Refactor the function into multiple modules (e.g., addition.py, multiplication.py) and call them from a main script.
+#Discuss why it is beneficial to organize code into modules.
+#Clean Organization: Each file does one thing.
+#Easier Debugging
+#Reusability: You can reuse addition.py in another project.
+#Different people can work on different modules-you separate responsibilities and avoid duplication
+
+def add(a, b):
+    return a + b
+
---
+++ modules/main.py
@@ -0,0 +1,11 @@
+#Import the functions into a new Python script and call them
+
+import math_operations
+from addition import add
+from multiplication import multiply
+
+#print(math_operations.multiply(4, 2))
+#print(math_operations.subtract(7, 1))
+
+print (add(10, 2))
+print (multiply(5, 7))
